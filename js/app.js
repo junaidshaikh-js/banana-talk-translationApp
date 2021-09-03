e2284a77bc3805f8c84963830871752159731258
Junaid <sj.shaikhjunaid@gmail.com>
2021-09-03 11:55:01 +0530

    feat: add app functionality

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const userInput = document.querySelector("#user-input");
+
+const translateBtn = document.querySelector(".translate-btn");
+
+const outputBox = document.querySelector(".translated-text");
+
+const URL = "https://api.funtranslations.com/translate/minion.json";
+
+function constructUrl(text) {
+  const textArr = text.split();
+  const urlString = `${URL}?text=${textArr.join("%20")}`;
+
+  return urlString;
+}
+
+const translateText = () => {
+  const userText = userInput.value;
+
+  if (!userText) {
+    alert("Please Enter Some Text");
+  }
+
+  const urlString = constructUrl(userText);
+
+  fetch(urlString)
+    .then((response) => response.json())
+    .then((data) => {
+      const translatedText = data.contents.translated;
+      outputBox.innerHTML = translatedText;
+    })
+    .catch(() => {
+      alert(`error occured: ${error} 
+        Please try again after some time.`);
+    });
+};
+
+translateBtn.addEventListener("click", translateText);
